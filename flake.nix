1e307bc033e4c6790ae1a724ba462d1589bbf570
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2024-05-15 11:06:07 +0300

    Pin nixpkgs and flake-utils inputs to avoid re-download for other projects

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/062ca2a9370a27a35c524dc82d540e6e9824b652";
+    flake-utils.url = "github:numtide/flake-utils/b1d9ab70662946ef0850d488da1c9019f3a9752a";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
